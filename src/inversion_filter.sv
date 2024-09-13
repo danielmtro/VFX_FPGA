4df229272a44c17d3f26b104b993c75c267a297a
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2024-09-13 12:47:43 +1000

    inversion filter complete and testbenched

--- src/inversion_filter.sv
+++ src/inversion_filter.sv
@@ -1,18 +1,25 @@
+/*
+Needs to be modified to take in 12 bit data. But I don't have a hex file of that for now
+*/
+
 module inversion_filter (
-	input 			clk,
+	input logic				clk,
 	
-	input[11:0] 	data_in,
-	output[11:0] 	data_out
+	input logic[11:0] 	data_in,
+	output logic[11:0] 	data_out,
+
+	input logic[2:0]		freq_flag //accomodates for 4 flags
+	
 
 );
 
-	logic [3:0] red, green, blue;
-	logic [3:0] red_inv, green_inv, blue_inv;
+	logic[3:0] red, green, blue;
+	logic[3:0] red_inv, green_inv, blue_inv;
 	
-	always_ff @(posedge clk) begin
-		red 	<= data_in[11:8];
-		green <= data_in[7:4];
-		blue  <= data_in[3:0];
+	always_comb begin
+		red 	= data_in[11:8];
+		green = data_in[7:4];
+		blue  = data_in[3:0];
 	end 
 	
 	always_comb begin
@@ -21,6 +28,11 @@ module inversion_filter (
 		blue_inv 	= 4'b1111 - blue;
 	end 
 	
-	assign data_out = {red_inv, green_inv, blue_inv};
+	always_comb begin
+		data_out = {red, green, blue};
+		if (freq_flag > 1) begin
+			data_out = {red_inv, green_inv, blue_inv};
+		end
+	end
 	
 endmodule 
