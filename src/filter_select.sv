b8e742aefd296d92d912a14c1748ddb65b31f0e8
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2024-09-18 15:46:12 +1000

    Create filter_select.sv

---
+++ src/filter_select.sv
@@ -0,0 +1,41 @@
+module filter_select(
+	input logic				clk,
+	
+	input logic[11:0] 	data_in,
+	output logic[11:0] 	data_out,
+	
+	input logic [2:0] filter_num,
+
+	input logic[2:0]		freq_flag //accomodates for 4 flags
+
+
+);
+	enum logic [1:0] {
+        COLOUR = 2'b00,
+        BLUR = 2'b01,
+        BRIGHTNESS = 2'b10,
+        EDGES = 2'b11
+    };
+
+	logic [11:0] inv_data, blur_data, bri_data, edge_data;
+
+	inversion_filter DUT (
+		.clk(clk),
+		.data_in(data_in),
+		.data_out(inv_data),
+		.freq_flag(freq_flag)
+	 );
+	 
+	 always_comb begin
+		
+		case (filter_num) 
+			COLOUR		: data_out = inv_data;
+			BLUR			: data_out = blur_data;
+			BRIGHTNESS 	: data_out = bri_data;
+			EDGES			: data_out = edge_data;
+		
+		
+	 end
+
+
+endmodule 
