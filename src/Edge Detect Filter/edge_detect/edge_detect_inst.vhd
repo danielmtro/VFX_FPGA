b636b5641a437418a46265d07f2ded97fe2f83ff
nickfreeborn <nfre8349@uni.sydney.edu.au>
2024-09-22 16:55:01 +1000

    Edge detect filter from platform designer

---
+++ src/Edge Detect Filter/edge_detect/edge_detect_inst.vhd
@@ -0,0 +1,29 @@
+	component edge_detect is
+		port (
+			clk_clk       : in  std_logic                    := 'X'; -- clk
+			reset_reset_n : in  std_logic                    := 'X'; -- reset_n
+			vga_CLK       : out std_logic;                           -- CLK
+			vga_HS        : out std_logic;                           -- HS
+			vga_VS        : out std_logic;                           -- VS
+			vga_BLANK     : out std_logic;                           -- BLANK
+			vga_SYNC      : out std_logic;                           -- SYNC
+			vga_R         : out std_logic_vector(7 downto 0);        -- R
+			vga_G         : out std_logic_vector(7 downto 0);        -- G
+			vga_B         : out std_logic_vector(7 downto 0)         -- B
+		);
+	end component edge_detect;
+
+	u0 : component edge_detect
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n, -- reset.reset_n
+			vga_CLK       => CONNECTED_TO_vga_CLK,       --   vga.CLK
+			vga_HS        => CONNECTED_TO_vga_HS,        --      .HS
+			vga_VS        => CONNECTED_TO_vga_VS,        --      .VS
+			vga_BLANK     => CONNECTED_TO_vga_BLANK,     --      .BLANK
+			vga_SYNC      => CONNECTED_TO_vga_SYNC,      --      .SYNC
+			vga_R         => CONNECTED_TO_vga_R,         --      .R
+			vga_G         => CONNECTED_TO_vga_G,         --      .G
+			vga_B         => CONNECTED_TO_vga_B          --      .B
+		);
+
---
+++ src/Edge Detect Filter/static_data_initialisation.sv
@@ -0,0 +1,63 @@
+module static_data_initialisation (
+    input  logic        clk,             
+    input  logic        reset,
+	input  logic        ready, 
+    
+    output logic [11:0] data,    // 12 bit data output
+    output logic        startofpacket,   // Start of packet signal
+    output logic        endofpacket,     // End of packet signal
+    output logic        valid            // valid output
+);
+
+
+    // Image ROMs:
+	 // The ram_init_file is a Quartus-only directive
+	//specifying the name of the initialisation file,
+	//and Verilator will ignore it.
+
+    (* ram_init_file = "chad-ho320x240.mif" *)  logic [2:0]  linear_grad [76800];
+
+    // The pixel counter/index. Set pixel_index_next in an always_comb block.
+    // Set pixel_index <= pixel_index_next in an always_ff block.
+    logic [18:0] pixel_index = 0, pixel_index_next; 
+ 
+    // Registers for reading from each ROM.
+    logic [2:0] linear_grad_q; 
+      
+    logic read_enable; // Need to have a read enable signal for the BRAM
+
+    // If reset, read the first pixel value. If valid&ready (handshake), read the next pixel value for the next handshake.
+    assign read_enable = reset | (valid & ready); 
+
+    always_ff @(posedge clk) begin : bram_read // This block is for correctly inferring BRAM in Quartus - we need read registers!
+        if (read_enable) begin
+            linear_grad_q   <= linear_grad[pixel_index_next];
+        end
+    end
+
+    logic [2:0] current_pixel; 
+
+    // set the current_pixel based on the reg value;
+    always_comb begin
+        current_pixel <= linear_grad_q;
+    end
+
+    assign valid = (reset) ? 0 : 1;                    // Data valid (constant)
+    assign startofpacket = (pixel_index == 0);         // Start of frame
+    assign endofpacket = (pixel_index == 76799); // End of frame
+
+    // set the data as the current pixel for output streaming
+    assign data = current_pixel;
+
+    assign pixel_index_next = (reset || pixel_index == 76799) ? 0 : pixel_index + 1;
+    
+    // flip flop to update the pixel_index value
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            pixel_index <= 0;
+        end else if (valid && ready) begin
+            pixel_index <= pixel_index_next;
+        end
+    end
+
+endmodule
