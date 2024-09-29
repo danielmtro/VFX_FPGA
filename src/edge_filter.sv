9ca69ede3339ffb2bef26b2baf20d2b2e5ef2b75
dinoplays <dinoplays21@gmail.com>
2024-09-29 19:24:46 +1000

    Fixed edge filter

--- src/edge_filter.sv
+++ src/edge_filter.sv
@@ -35,11 +35,11 @@ module edge_filter (
     logic [3:0] green_buffer [0:(image_width*4 + 4)];
     logic [3:0] blue_buffer [0:(image_width*4 + 4)];
 
-    logic signed [9:0] partial_sum_r_stage1 [4:0], partial_sum_g_stage1 [4:0], partial_sum_b_stage1 [4:0];
-    logic signed [9:0] partial_sum_r_stage2 [4:0], partial_sum_g_stage2 [4:0], partial_sum_b_stage2 [4:0];
-    logic signed [9:0] partial_sum_r_stage3 [4:0], partial_sum_g_stage3 [4:0], partial_sum_b_stage3 [4:0];
+    logic signed [11:0] partial_sum_r_stage1 [4:0], partial_sum_g_stage1 [4:0], partial_sum_b_stage1 [4:0];
+    logic signed [11:0] partial_sum_r_stage2 [4:0], partial_sum_g_stage2 [4:0], partial_sum_b_stage2 [4:0];
+    logic signed [11:0] partial_sum_r_stage3 [4:0], partial_sum_g_stage3 [4:0], partial_sum_b_stage3 [4:0];
 
-    logic signed [9:0] conv_result_r, conv_result_g, conv_result_b;  // Final convolution results for RGB
+    logic signed [11:0] conv_result_r, conv_result_g, conv_result_b;  // Final convolution results for RGB
 
     // Define the kernel weights
     logic signed [2:0] kernel [0:4][0:4];
