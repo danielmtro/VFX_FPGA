ca54d6843f0734df094130bae11318cb3cab1e84
dinoplays <dinoplays21@gmail.com>
2024-10-02 12:04:30 +1000

    Changed kernels to be 2d

--- src/blurring_filter.sv
+++ src/blurring_filter.sv
@@ -41,48 +41,48 @@ module blurring_filter (
     logic [9:0] conv_result_r, conv_result_g, conv_result_b;  // Final convolution results for RGB
 
     // Define the bitshift kernel
-    logic [3:0] kernel [0:4][0:6];
+    logic [3:0] kernel [0:34];
 
     always_comb begin
-        kernel[0][0] = 15;
-        kernel[0][1] = 15;
-        kernel[0][2] = 0;
-        kernel[0][3] = 0;
-        kernel[0][4] = 0;
-        kernel[0][5] = 1;
-        kernel[0][6] = 1;
+        kernel[0] = 15;
+        kernel[1] = 15;
+        kernel[2] = 0;
+        kernel[3] = 0;
+        kernel[4] = 0;
+        kernel[5] = 1;
+        kernel[6] = 1;
 
-        kernel[1][0] = 15;
-        kernel[1][1] = 0;
-        kernel[1][2] = 0;
-        kernel[1][3] = 0;
-        kernel[1][4] = 1;
-        kernel[1][5] = 1;
-        kernel[1][6] = 1;
+        kernel[7] = 15;
+        kernel[8] = 0;
+        kernel[9] = 0;
+        kernel[10] = 0;
+        kernel[11] = 1;
+        kernel[12] = 1;
+        kernel[13] = 1;
 
-        kernel[2][0] = 0;
-        kernel[2][1] = 0;
-        kernel[2][2] = 0;
-        kernel[2][3] = 1;
-        kernel[2][4] = 1;
-        kernel[2][5] = 1;
-        kernel[2][6] = 2;
+        kernel[14] = 0;
+        kernel[15] = 0;
+        kernel[16] = 0;
+        kernel[17] = 1;
+        kernel[18] = 1;
+        kernel[19] = 1;
+        kernel[20] = 2;
 
-        kernel[3][0] = 0;
-        kernel[3][1] = 0;
-        kernel[3][2] = 1;
-        kernel[3][3] = 1;
-        kernel[3][4] = 1;
-        kernel[3][5] = 2;
-        kernel[3][6] = 2;
+        kernel[21] = 0;
+        kernel[22] = 0;
+        kernel[23] = 1;
+        kernel[24] = 1;
+        kernel[25] = 1;
+        kernel[26] = 2;
+        kernel[27] = 2;
 
-        kernel[4][0] = 0;
-        kernel[4][1] = 1;
-        kernel[4][2] = 1;
-        kernel[4][3] = 1;
-        kernel[4][4] = 2;
-        kernel[4][5] = 2;
-        kernel[4][6] = 2;
+        kernel[28] = 0;
+        kernel[29] = 1;
+        kernel[30] = 1;
+        kernel[31] = 1;
+        kernel[32] = 2;
+        kernel[33] = 2;
+        kernel[34] = 2;
 
         /*
         Sum is 64
@@ -124,113 +124,113 @@ module blurring_filter (
         conv_result_g <= 0;
         conv_result_b <= 0;
 
-        conv_result_r <= (red_buffer[(0 * image_width) + 0] << kernel[0][0])
-                        + (red_buffer[(0 * image_width) + 1] << kernel[0][1])
-                        + (red_buffer[(0 * image_width) + 2] << kernel[0][2])
-                        + (red_buffer[(0 * image_width) + 3] << kernel[0][3])
-                        + (red_buffer[(0 * image_width) + 4] << kernel[0][4])
-                        + (red_buffer[(0 * image_width) + 5] << kernel[0][5])
-                        + (red_buffer[(0 * image_width) + 6] << kernel[0][6])
-                        + (red_buffer[(1 * image_width) + 0] << kernel[1][0])
-                        + (red_buffer[(1 * image_width) + 1] << kernel[1][1])
-                        + (red_buffer[(1 * image_width) + 2] << kernel[1][2])
-                        + (red_buffer[(1 * image_width) + 3] << kernel[1][3])
-                        + (red_buffer[(1 * image_width) + 4] << kernel[1][4])
-                        + (red_buffer[(1 * image_width) + 5] << kernel[1][5])
-                        + (red_buffer[(1 * image_width) + 6] << kernel[1][6])
-                        + (red_buffer[(2 * image_width) + 0] << kernel[2][0])
-                        + (red_buffer[(2 * image_width) + 1] << kernel[2][1])
-                        + (red_buffer[(2 * image_width) + 2] << kernel[2][2])
-                        + (red_buffer[(2 * image_width) + 3] << kernel[2][3])
-                        + (red_buffer[(2 * image_width) + 4] << kernel[2][4])
-                        + (red_buffer[(2 * image_width) + 5] << kernel[2][5])
-                        + (red_buffer[(2 * image_width) + 6] << kernel[2][6])
-                        + (red_buffer[(3 * image_width) + 0] << kernel[3][0])
-                        + (red_buffer[(3 * image_width) + 1] << kernel[3][1])
-                        + (red_buffer[(3 * image_width) + 2] << kernel[3][2])
-                        + (red_buffer[(3 * image_width) + 3] << kernel[3][3])
-                        + (red_buffer[(3 * image_width) + 4] << kernel[3][4])
-                        + (red_buffer[(3 * image_width) + 5] << kernel[3][5])
-                        + (red_buffer[(3 * image_width) + 6] << kernel[3][6])
-                        + (red_buffer[(4 * image_width) + 0] << kernel[4][0])
-                        + (red_buffer[(4 * image_width) + 1] << kernel[4][1])
-                        + (red_buffer[(4 * image_width) + 2] << kernel[4][2])
-                        + (red_buffer[(4 * image_width) + 3] << kernel[4][3])
-                        + (red_buffer[(4 * image_width) + 4] << kernel[4][4])
-                        + (red_buffer[(4 * image_width) + 5] << kernel[4][5])
-                        + (red_buffer[(4 * image_width) + 6] << kernel[4][6]);
+        conv_result_r <= (red_buffer[(0 * image_width) + 0] << kernel[0])
+                        + (red_buffer[(0 * image_width) + 1] << kernel[1])
+                        + (red_buffer[(0 * image_width) + 2] << kernel[2])
+                        + (red_buffer[(0 * image_width) + 3] << kernel[3])
+                        + (red_buffer[(0 * image_width) + 4] << kernel[4])
+                        + (red_buffer[(0 * image_width) + 5] << kernel[5])
+                        + (red_buffer[(0 * image_width) + 6] << kernel[6])
+                        + (red_buffer[(1 * image_width) + 0] << kernel[7])
+                        + (red_buffer[(1 * image_width) + 1] << kernel[8])
+                        + (red_buffer[(1 * image_width) + 2] << kernel[9])
+                        + (red_buffer[(1 * image_width) + 3] << kernel[10])
+                        + (red_buffer[(1 * image_width) + 4] << kernel[11])
+                        + (red_buffer[(1 * image_width) + 5] << kernel[12])
+                        + (red_buffer[(1 * image_width) + 6] << kernel[13])
+                        + (red_buffer[(2 * image_width) + 0] << kernel[14])
+                        + (red_buffer[(2 * image_width) + 1] << kernel[15])
+                        + (red_buffer[(2 * image_width) + 2] << kernel[16])
+                        + (red_buffer[(2 * image_width) + 3] << kernel[17])
+                        + (red_buffer[(2 * image_width) + 4] << kernel[18])
+                        + (red_buffer[(2 * image_width) + 5] << kernel[19])
+                        + (red_buffer[(2 * image_width) + 6] << kernel[20])
+                        + (red_buffer[(3 * image_width) + 0] << kernel[21])
+                        + (red_buffer[(3 * image_width) + 1] << kernel[22])
+                        + (red_buffer[(3 * image_width) + 2] << kernel[23])
+                        + (red_buffer[(3 * image_width) + 3] << kernel[24])
+                        + (red_buffer[(3 * image_width) + 4] << kernel[25])
+                        + (red_buffer[(3 * image_width) + 5] << kernel[26])
+                        + (red_buffer[(3 * image_width) + 6] << kernel[27])
+                        + (red_buffer[(4 * image_width) + 0] << kernel[28])
+                        + (red_buffer[(4 * image_width) + 1] << kernel[29])
+                        + (red_buffer[(4 * image_width) + 2] << kernel[30])
+                        + (red_buffer[(4 * image_width) + 3] << kernel[31])
+                        + (red_buffer[(4 * image_width) + 4] << kernel[32])
+                        + (red_buffer[(4 * image_width) + 5] << kernel[33])
+                        + (red_buffer[(4 * image_width) + 6] << kernel[34]);
         
-        conv_result_g <= (green_buffer[(0 * image_width) + 0] << kernel[0][0])
-                        + (green_buffer[(0 * image_width) + 1] << kernel[0][1])
-                        + (green_buffer[(0 * image_width) + 2] << kernel[0][2])
-                        + (green_buffer[(0 * image_width) + 3] << kernel[0][3])
-                        + (green_buffer[(0 * image_width) + 4] << kernel[0][4])
-                        + (green_buffer[(0 * image_width) + 5] << kernel[0][5])
-                        + (green_buffer[(0 * image_width) + 6] << kernel[0][6])
-                        + (green_buffer[(1 * image_width) + 0] << kernel[1][0])
-                        + (green_buffer[(1 * image_width) + 1] << kernel[1][1])
-                        + (green_buffer[(1 * image_width) + 2] << kernel[1][2])
-                        + (green_buffer[(1 * image_width) + 3] << kernel[1][3])
-                        + (green_buffer[(1 * image_width) + 4] << kernel[1][4])
-                        + (green_buffer[(1 * image_width) + 5] << kernel[1][5])
-                        + (green_buffer[(1 * image_width) + 6] << kernel[1][6])
-                        + (green_buffer[(2 * image_width) + 0] << kernel[2][0])
-                        + (green_buffer[(2 * image_width) + 1] << kernel[2][1])
-                        + (green_buffer[(2 * image_width) + 2] << kernel[2][2])
-                        + (green_buffer[(2 * image_width) + 3] << kernel[2][3])
-                        + (green_buffer[(2 * image_width) + 4] << kernel[2][4])
-                        + (green_buffer[(2 * image_width) + 5] << kernel[2][5])
-                        + (green_buffer[(2 * image_width) + 6] << kernel[2][6])
-                        + (green_buffer[(3 * image_width) + 0] << kernel[3][0])
-                        + (green_buffer[(3 * image_width) + 1] << kernel[3][1])
-                        + (green_buffer[(3 * image_width) + 2] << kernel[3][2])
-                        + (green_buffer[(3 * image_width) + 3] << kernel[3][3])
-                        + (green_buffer[(3 * image_width) + 4] << kernel[3][4])
-                        + (green_buffer[(3 * image_width) + 5] << kernel[3][5])
-                        + (green_buffer[(3 * image_width) + 6] << kernel[3][6])
-                        + (green_buffer[(4 * image_width) + 0] << kernel[4][0])
-                        + (green_buffer[(4 * image_width) + 1] << kernel[4][1])
-                        + (green_buffer[(4 * image_width) + 2] << kernel[4][2])
-                        + (green_buffer[(4 * image_width) + 3] << kernel[4][3])
-                        + (green_buffer[(4 * image_width) + 4] << kernel[4][4])
-                        + (green_buffer[(4 * image_width) + 5] << kernel[4][5])
-                        + (green_buffer[(4 * image_width) + 6] << kernel[4][6]);
+        conv_result_g <= (green_buffer[(0 * image_width) + 0] << kernel[0])
+                        + (green_buffer[(0 * image_width) + 1] << kernel[1])
+                        + (green_buffer[(0 * image_width) + 2] << kernel[2])
+                        + (green_buffer[(0 * image_width) + 3] << kernel[3])
+                        + (green_buffer[(0 * image_width) + 4] << kernel[4])
+                        + (green_buffer[(0 * image_width) + 5] << kernel[5])
+                        + (green_buffer[(0 * image_width) + 6] << kernel[6])
+                        + (green_buffer[(1 * image_width) + 0] << kernel[7])
+                        + (green_buffer[(1 * image_width) + 1] << kernel[8])
+                        + (green_buffer[(1 * image_width) + 2] << kernel[9])
+                        + (green_buffer[(1 * image_width) + 3] << kernel[10])
+                        + (green_buffer[(1 * image_width) + 4] << kernel[11])
+                        + (green_buffer[(1 * image_width) + 5] << kernel[12])
+                        + (green_buffer[(1 * image_width) + 6] << kernel[13])
+                        + (green_buffer[(2 * image_width) + 0] << kernel[14])
+                        + (green_buffer[(2 * image_width) + 1] << kernel[15])
+                        + (green_buffer[(2 * image_width) + 2] << kernel[16])
+                        + (green_buffer[(2 * image_width) + 3] << kernel[17])
+                        + (green_buffer[(2 * image_width) + 4] << kernel[18])
+                        + (green_buffer[(2 * image_width) + 5] << kernel[19])
+                        + (green_buffer[(2 * image_width) + 6] << kernel[20])
+                        + (green_buffer[(3 * image_width) + 0] << kernel[21])
+                        + (green_buffer[(3 * image_width) + 1] << kernel[22])
+                        + (green_buffer[(3 * image_width) + 2] << kernel[23])
+                        + (green_buffer[(3 * image_width) + 3] << kernel[24])
+                        + (green_buffer[(3 * image_width) + 4] << kernel[25])
+                        + (green_buffer[(3 * image_width) + 5] << kernel[26])
+                        + (green_buffer[(3 * image_width) + 6] << kernel[27])
+                        + (green_buffer[(4 * image_width) + 0] << kernel[28])
+                        + (green_buffer[(4 * image_width) + 1] << kernel[29])
+                        + (green_buffer[(4 * image_width) + 2] << kernel[30])
+                        + (green_buffer[(4 * image_width) + 3] << kernel[31])
+                        + (green_buffer[(4 * image_width) + 4] << kernel[32])
+                        + (green_buffer[(4 * image_width) + 5] << kernel[33])
+                        + (green_buffer[(4 * image_width) + 6] << kernel[34]);
 
-        conv_result_b <= (blue_buffer[(0 * image_width) + 0] << kernel[0][0])
-                        + (blue_buffer[(0 * image_width) + 1] << kernel[0][1])
-                        + (blue_buffer[(0 * image_width) + 2] << kernel[0][2])
-                        + (blue_buffer[(0 * image_width) + 3] << kernel[0][3])
-                        + (blue_buffer[(0 * image_width) + 4] << kernel[0][4])
-                        + (blue_buffer[(0 * image_width) + 5] << kernel[0][5])
-                        + (blue_buffer[(0 * image_width) + 6] << kernel[0][6])
-                        + (blue_buffer[(1 * image_width) + 0] << kernel[1][0])
-                        + (blue_buffer[(1 * image_width) + 1] << kernel[1][1])
-                        + (blue_buffer[(1 * image_width) + 2] << kernel[1][2])
-                        + (blue_buffer[(1 * image_width) + 3] << kernel[1][3])
-                        + (blue_buffer[(1 * image_width) + 4] << kernel[1][4])
-                        + (blue_buffer[(1 * image_width) + 5] << kernel[1][5])
-                        + (blue_buffer[(1 * image_width) + 6] << kernel[1][6])
-                        + (blue_buffer[(2 * image_width) + 0] << kernel[2][0])
-                        + (blue_buffer[(2 * image_width) + 1] << kernel[2][1])
-                        + (blue_buffer[(2 * image_width) + 2] << kernel[2][2])
-                        + (blue_buffer[(2 * image_width) + 3] << kernel[2][3])
-                        + (blue_buffer[(2 * image_width) + 4] << kernel[2][4])
-                        + (blue_buffer[(2 * image_width) + 5] << kernel[2][5])
-                        + (blue_buffer[(2 * image_width) + 6] << kernel[2][6])
-                        + (blue_buffer[(3 * image_width) + 0] << kernel[3][0])
-                        + (blue_buffer[(3 * image_width) + 1] << kernel[3][1])
-                        + (blue_buffer[(3 * image_width) + 2] << kernel[3][2])
-                        + (blue_buffer[(3 * image_width) + 3] << kernel[3][3])
-                        + (blue_buffer[(3 * image_width) + 4] << kernel[3][4])
-                        + (blue_buffer[(3 * image_width) + 5] << kernel[3][5])
-                        + (blue_buffer[(3 * image_width) + 6] << kernel[3][6])
-                        + (blue_buffer[(4 * image_width) + 0] << kernel[4][0])
-                        + (blue_buffer[(4 * image_width) + 1] << kernel[4][1])
-                        + (blue_buffer[(4 * image_width) + 2] << kernel[4][2])
-                        + (blue_buffer[(4 * image_width) + 3] << kernel[4][3])
-                        + (blue_buffer[(4 * image_width) + 4] << kernel[4][4])
-                        + (blue_buffer[(4 * image_width) + 5] << kernel[4][5])
-                        + (blue_buffer[(4 * image_width) + 6] << kernel[4][6]);
+        conv_result_b <= (blue_buffer[(0 * image_width) + 0] << kernel[0])
+                        + (blue_buffer[(0 * image_width) + 1] << kernel[1])
+                        + (blue_buffer[(0 * image_width) + 2] << kernel[2])
+                        + (blue_buffer[(0 * image_width) + 3] << kernel[3])
+                        + (blue_buffer[(0 * image_width) + 4] << kernel[4])
+                        + (blue_buffer[(0 * image_width) + 5] << kernel[5])
+                        + (blue_buffer[(0 * image_width) + 6] << kernel[6])
+                        + (blue_buffer[(1 * image_width) + 0] << kernel[7])
+                        + (blue_buffer[(1 * image_width) + 1] << kernel[8])
+                        + (blue_buffer[(1 * image_width) + 2] << kernel[9])
+                        + (blue_buffer[(1 * image_width) + 3] << kernel[10])
+                        + (blue_buffer[(1 * image_width) + 4] << kernel[11])
+                        + (blue_buffer[(1 * image_width) + 5] << kernel[12])
+                        + (blue_buffer[(1 * image_width) + 6] << kernel[13])
+                        + (blue_buffer[(2 * image_width) + 0] << kernel[14])
+                        + (blue_buffer[(2 * image_width) + 1] << kernel[15])
+                        + (blue_buffer[(2 * image_width) + 2] << kernel[16])
+                        + (blue_buffer[(2 * image_width) + 3] << kernel[17])
+                        + (blue_buffer[(2 * image_width) + 4] << kernel[18])
+                        + (blue_buffer[(2 * image_width) + 5] << kernel[19])
+                        + (blue_buffer[(2 * image_width) + 6] << kernel[20])
+                        + (blue_buffer[(3 * image_width) + 0] << kernel[21])
+                        + (blue_buffer[(3 * image_width) + 1] << kernel[22])
+                        + (blue_buffer[(3 * image_width) + 2] << kernel[23])
+                        + (blue_buffer[(3 * image_width) + 3] << kernel[24])
+                        + (blue_buffer[(3 * image_width) + 4] << kernel[25])
+                        + (blue_buffer[(3 * image_width) + 5] << kernel[26])
+                        + (blue_buffer[(3 * image_width) + 6] << kernel[27])
+                        + (blue_buffer[(4 * image_width) + 0] << kernel[28])
+                        + (blue_buffer[(4 * image_width) + 1] << kernel[29])
+                        + (blue_buffer[(4 * image_width) + 2] << kernel[30])
+                        + (blue_buffer[(4 * image_width) + 3] << kernel[31])
+                        + (blue_buffer[(4 * image_width) + 4] << kernel[32])
+                        + (blue_buffer[(4 * image_width) + 5] << kernel[33])
+                        + (blue_buffer[(4 * image_width) + 6] << kernel[34]);
 
         if (ready_in && valid_in) begin
             if (is_underage) begin
