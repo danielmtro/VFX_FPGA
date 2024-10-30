0ff6973350bc9c9f52426b27ff2afb749d28f4f5
dinoplays <166807132+dinoplays@users.noreply.github.com>
2024-10-31 10:38:23 +1100

    Delete src directory

--- src/StaticImageTesting/synchroniser.sv
+++
@@ -1,9 +0,0 @@
-module synchroniser (input clk, x, output y);
-    reg x_q0, x_q1;
-    always @(posedge clk)
-    begin
-        x_q0 <= x;    // Flip-flop #1
-        x_q1 <= x_q0; // Flip-flop #2
-    end
-    assign y = x_q1;
-endmodule
